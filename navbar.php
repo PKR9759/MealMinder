46cd33e5b55c9e5eb57f70022d29a419f34d7159
pkr <kuldiprparmar9759@gmail.com>
2023-04-15 15:30:05 +0530

    sql file added

--- navbar.php
+++ navbar.php
@@ -1,26 +1,4 @@
-<!doctype html>
-<html lang="en">
 
-<head>
-    <style>
-        .navbar-nav .nav-link {
-  color: #fff;
-  
-}
-    </style>
-
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-    <title>navbar</title>
-</head>
-
-<body>
     <nav class="navbar navbar-expand-lg " style="background-color:#EC451C ;">
         <a class="navbar-brand" style=" color:aliceblue" href="#">Navbar</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
@@ -28,18 +6,18 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav">
+            <ul class="navbar-nav" style="color:white">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-link"  style="color:white" href="#">Home <span class="sr-only">(current)</span></a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Feedback</a>
+                <li class="nav-item" >
+                    <a class="nav-link"style="color:white" href="#">Feedback</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
+                <li class="nav-item" >
+                    <a class="nav-link" style="color:white" href="#">About</a>
                 </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                <li class="nav-item dropdown" >
+                    <a class="nav-link dropdown-toggle" style="color:white" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown"
                         aria-haspopup="true" aria-expanded="false">
                         Catagory
                     </a>
@@ -53,6 +31,20 @@
         </div>
     </nav>
 
+
+
+    
+
+
+
+    <!-- write code from here -->
+
+
+
+
+
+
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
@@ -64,6 +56,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
         crossorigin="anonymous"></script>
-</body>
+
 
 </html>
