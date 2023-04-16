25e24070967592c8e5f59f15f884e929db270fc7
lakhman108 <119241790+lakhman108@users.noreply.github.com>
2023-04-16 11:17:29 +0530

    smaple category made

---
+++ homepage/category's.php
@@ -0,0 +1,52 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+   <style>
+     .card-group {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+
+.card {
+  flex-basis: calc(33.33% - 10px);
+  margin-bottom: 20px;
+}
+
+.card-img-top {
+  width: 100%;
+  height: 200px;
+  object-fit: cover;
+}
+.h2{
+  text-align: center;
+  color: rgb(7, 28, 111);
+}
+   </style>
+
+</head>
+<body>
+  <h1 class="h2">GUJARATI FOOD</h1>
+<div class="card-group">
+  <div class="card">
+    <img src="food1.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h2 class="card-title">dhokala</h2>
+      
+  <div class="card">
+    <img src="food2.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h2 class="card-title">Dhokala</h2>
+     
+  <div class="card">
+    <img src="food1.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h2 class="card-title">Thepla</h2>
+  
+  </div> 
+</div>
+</body>
+</html>
