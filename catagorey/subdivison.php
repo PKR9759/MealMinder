0718e354ea5216ba7306c853c9ae46e8b5506e11
lakhman108 <119241790+lakhman108@users.noreply.github.com>
2023-04-16 11:00:53 +0530

    cheking

--- catagorey/subdivison.php
+++ catagorey/subdivison.php
@@ -1,77 +1,29 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Table Example</title>
-    <style>
-      table {
-        border-collapse: collapse;
-        width: 100%;
-      }
-      th, td {
-        padding: 8px;
-        text-align: left;
-        border-bottom: 1px solid #ddd;
-      }
-      th {
-        background-color: #4CAF50;
-        color: white;
-      }
-      tr:nth-child(even) {
-        background-color: #f2f2f2;
-      }
-      tr:hover {
-  background-color: orange;
-}
-
-      
-    </style>
-  </head>
-  <body>
-   
-
-    <?php
-    //connecting to the database
-    $servername = "localhost";
-    $username="root";
-    $password="";
-    $database="rbh";
-    //creating a connection
-$conn = mysqli_connect($servername,$username,$password,$database);
-
-if (!$conn) {
-    die("sorry we failed to connect:" . mysqli_connect_error());
-} 
-
-$sql = "SELECT * FROM `items`";
-$result = mysqli_query($conn,$sql);
-
-//find the number of records if greater than zero we will do further process
-$num = mysqli_num_rows($result);
-if ($num) {
-    // start of table
-    echo "<table >";
-    
-    // table header row
-    echo "<tr>";
-    echo "<th>Sr No.</th>";
-    echo "<th>Name</th>";
-    echo "<th>photo</th>";
-    
-    echo "</tr>";
-    
-    // table data rows
-    while ($row = mysqli_fetch_assoc($result)) {
-        echo "<tr>";
-        echo "<td>" . $row['id'] . "</td>";
-        echo "<td>" . $row['name'] . "</td>";
-        echo "<td><img src='" . $row['photo'] . "' alt='item photo'></td>";
-        
-        echo "</tr>";
-    }
-    
-    // end of table
-    echo "</table>";
-}
+<?php
+error_reporting(0);
 ?>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="" method="POST" enctype="multipart/form-data">
+  <input type="file" name="uploadfile" id="uploadfile">
+  <input type="submit" name="submit"value="upload file">
+  </form>
 </body>
 </html>
+
+<?php
+
+ $filename  = $_FILES["uploadfile"]["name"];
+ $tempname = $_FILES["uploadfile"]["tmp_name"];
+ 
+ 
+ $folder="student/".$filename;
+ 
+ move_uploaded_file($tempname,$folder);
+
+?>
