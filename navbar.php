5aabf78818039945570ac292a4ca72f075ec006f
lakhman108 <119241790+lakhman108@users.noreply.github.com>
2023-04-17 21:17:58 +0530

    navbar

--- navbar.php
+++ navbar.php
@@ -1,8 +1,18 @@
+<head>
+    
+    <!-- Add the Tailwind CSS CDN -->
+  
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
 <style>.idea li{
         padding: 15px;
         height: 50%;
     }
+    .
     .idea{
+        
         font-size: 30px;
      color: whitesmoke;
         background-color: #1f242d;
