114b55640d28d70a3e454359da184c771e8560bf
lakhman108 <119241790+lakhman108@users.noreply.github.com>
2023-04-27 22:25:39 +0530

    i added a form to uload cards and made table which you need to add in rbh

--- catagory/gujrati.php
+++ catagory/gujrati.php
@@ -1,36 +1,31 @@
 <?php
 include 'C:\xampp\htdocs\rbh\navbar.php';
 ?>
+
 <!doctype html>
 <html lang="en">
 
 <head>
     <style>
-       .recsec{
-      display: flex;
-      flex-wrap: wrap;
-      justify-content:space-around;
-      /* grid-template-rows: repeat(4,1fr); */
-      /* flex-wrap: wrap; */
-      
-      
-      
-    }
-    .card{
-      margin-top:20px;
-      border-radius: 20px; 
-      
-    }
+        .recsec {
+            display: flex;
+            flex-wrap: wrap;
+            /* justify-content: space-around; */
+        }
+
+        .card {
+            margin-top: 20px;
+            border-radius: 20px;
+        }
     </style>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
     <title>guj</title>
 </head>
 
@@ -38,100 +33,52 @@ include 'C:\xampp\htdocs\rbh\navbar.php';
     <br>
     <div class="jumbotron" style="background-color:#F17766">
         <h1 class="display-4">Gujrati Dishes</h1>
-        
         <hr class="my-3" style="font-family:'Lobster',sans-serif">
-        <p>Whether you are a lifelong fan of Gujarati cuisine or trying it for the first time, our site  is the perfect place to savor the flavors and textures of this unique regional cuisine. So come and join us for a culinary journey through the tastes and traditions of Gujarat!</p>
-        
+        <p>Whether you are a lifelong fan of Gujarati cuisine or trying it for the first time, our site is the perfect place to savor the flavors and textures of this unique regional cuisine. So come and join us for a culinary journey through the tastes and traditions of Gujarat!</p>
     </div>
-    <h4>Chose Your food and cold-drinks ...<h4>
+    <h4>Choose Your food and cold-drinks ...</h4>
     <hr>
     <div class="recsec">
 
-    <div class="card" style="width: 18rem;">
-      <img class="card-img-top" src="https://source.unsplash.com/random/?Gujratifood-circle" alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-        content.</p>
+    <?php
+//connecting to the database
+$servername = "localhost";
+$username="root";
+$password="";
+$database="RBH";
+//creating a connection
+$conn = mysqli_connect($servername,$username,$password,$database);
 
-      </div>
-    </div>
+if (!$conn) {
+    die("sorry we failed to connect:" . mysqli_connect_error());
+} else {
+    echo "connected successfully  ";
+    echo "<br>";
+}
 
-    <div class="card" style="width: 18rem;">
-      <img class="card-img-top" src="https://source.unsplash.com/random/?Gujratifood-circle" alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-        content.</p>
-      </div>
-    </div>
+$sql = "SELECT * FROM `files`";
+$result = mysqli_query($conn,$sql);
 
-    <div class="card" style="width: 18rem;">
-      <img class="card-img-top" src="https://source.unsplash.com/random/?Gujratifood-circle" alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-          content.</p>
+//find the number of records if greater than zero we will do further process
+$num = mysqli_num_rows($result);
+if ($num) {
+    // start of container
+    echo '<div class="recsec" >';
 
-      </div>
-    </div>
-
-    <div class="card" style="width: 18rem;">
-      <img class="card-img-top" src="https://source.unsplash.com/random/?Gujratifood-circle" alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-          content.</p>
-
-      </div>
-    </div>
-
-    <div class="card" style="width: 18rem;">
-      <img class="card-img-top" src="https://source.unsplash.com/random/?Gujratifood-circle" alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-          content.</p>
-
-      </div>
-    </div>
-
-    <div class="card" style="width: 18rem;">
-      <img class="card-img-top" src="https://source.unsplash.com/random/?Gujratifood-circle" alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-          content.</p>
-
-      </div>
-    </div>
-
-    <div class="card" style="width: 18rem;">
-      <img class="card-img-top" src="https://source.unsplash.com/random/?Gujratifood-circle" alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-          content.</p>
-
-      </div>
-    </div>
-
-    <div class="card" style="width: 18rem;">
-      <img class="card-img-top" src="https://source.unsplash.com/random/?Gujratifood-circle" alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-          content.</p>
-
-      </div>
-    </div>
-
-  </div>
-
-
-
-
-
+    // loop through the records and create a card for each one
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo '<div class="card" style="width: 18rem;">';
+        echo '<img class="card-img-top" src="' . $row['file_path'] . '" alt="' . $row['name'] . '">';
+        echo '<div class="card-body">';
+        echo '<h5 class="card-title">' . $row['name'] . '</h5>';
+        echo '<p class="card-text">' . $row['price'] . '</p>';
+        echo '</div>';
+        echo '</div>';
+    }
+ 
+    echo '</div>';
+}
+?>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
@@ -143,6 +90,6 @@ include 'C:\xampp\htdocs\rbh\navbar.php';
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
         crossorigin="anonymous"></script>
-</body>
-
+  </body>
 </html>
+
