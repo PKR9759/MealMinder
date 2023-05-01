00e6237aab46aafcbb719177acd5f542e07179a5
lakhman108 <119241790+lakhman108@users.noreply.github.com>
2023-05-01 22:54:55 +0530

    ok

---
+++ catagory/chinese.php
@@ -0,0 +1,90 @@
+<?php
+include '/Applications/XAMPP/xamppfiles/htdocs/food2/navbar.php';
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+<link rel="stylesheet" href="cat.css">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>chinese</title>
+</head>
+
+<body>
+    <br>
+    <div class="jumbotron" style="background-color:#F17766">
+        <h1 class="display-4">Chinese Foods</h1>
+        
+        <hr class="my-3" style="font-family:'Lobster',sans-serif">
+        <p>Savor the flavors of China with our authentic and delicious Chinese cuisine. From spicy Sichuan dishes to savory Cantonese classics, our menu features a wide variety of dishes that will transport your taste buds to the streets of China. Made with the freshest and highest quality ingredients, our dishes are cooked to perfection using authentic cooking techniques. So come and experience the rich and varied flavors of China at our site today.</p>
+        
+    </div>
+    <h4>Chose Your food and cold-drinks ...<h4>
+    <hr>
+    <?php
+//connecting to the database
+$servername = "localhost";
+$username="root";
+$password="";
+$database="RBH";
+//creating a connection
+$conn = mysqli_connect($servername,$username,$password,$database);
+
+if (!$conn) {
+    die("sorry we failed to connect:" . mysqli_connect_error());
+} else {
+    echo "connected successfully  ";
+    echo "<br>";
+}
+
+$sql = "SELECT * FROM `chinese_items`";
+$result = mysqli_query($conn,$sql);
+
+//find the number of records if greater than zero we will do further process
+$num = mysqli_num_rows($result);
+if ($num) {
+    // start of container
+    echo '<div class="recsec" >';
+
+    // loop through the records and create a card for each one
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo '<div class="card" style="width: 18rem;">';
+        echo '<img class="card-img-top" src="' . $row['file_path'] . '" alt="' . $row['name'] . '">';
+        echo '<div class="card-body">';
+        echo '<h5 class="card-title">' . $row['name'] . '</h5>';
+        if ($row['aviability'] == 1) {
+            echo '<p class="card-text"><span class="stock green">In Stock</span> - Price: ' . $row['price'] . '</p>';
+        } else {
+            echo '<p class="card-text"><span class="stock red">Out of Stock</span></p>';
+        }
+        
+        
+        echo '</div>';
+        echo '</div>';
+    }
+ 
+    echo '</div>';
+}
+?>
+
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ catagory/upload.php
@@ -0,0 +1,84 @@
+<?php    
+// Database connection
+include "config.php";
+
+if (isset($_POST["submit"])) {
+    // Set image placement folder 
+    $target_dir = "img_dir/";
+    // Get file path
+    $target_file = $target_dir . basename($_FILES["fileUpload"]["name"]);
+    // Get file extension
+    $imageExt = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+    // Allowed file types
+    $allowd_file_ext = array("jpg", "jpeg", "png");
+
+    if (!file_exists($_FILES["fileUpload"]["tmp_name"])) {
+        $resMessage = array(
+            "message" => "Select image to upload.",
+        );
+    } else if (!in_array($imageExt, $allowd_file_ext)) {
+        $resMessage = array(
+            "message" => "Allowed file formats .jpg, .jpeg and .png.",
+        );
+    } else if ($_FILES["fileUpload"]["size"] > 209715234) {
+        $resMessage = array(
+            "message" => "File is too large. File size should be less than 2 megabytes.",
+        );
+    } else if (file_exists($target_file)) {
+        $resMessage = array(
+            "message" => "File already exists.",
+        );
+    } else {
+        if (move_uploaded_file($_FILES["fileUpload"]["tmp_name"], $target_file)) {
+            $name = $_POST["name"];
+            $price = $_POST["price"];
+            $description = $_POST["description"];
+            $aviability = $_POST["aviability"];
+            $category = $_POST["category"];
+            $param_path = $target_file;
+
+            if ($category == 1) {
+                $sql = "INSERT INTO `Gujarati_items` (`id`, `file_path`, `name`, `price`, `description`, `aviability`) 
+                        VALUES (NULL, ?, ?, ?, ?, ?)";
+            } else if ($category == 2) {
+                $sql = "INSERT INTO `Punjabi_items` (`id`, `file_path`, `name`, `price`, `description`, `aviability`) 
+                        VALUES (NULL, ?, ?, ?, ?, ?)";
+            }
+            else if ($category == 3) {
+                $sql = "INSERT INTO `chinese_items` (`id`, `file_path`, `name`, `price`, `description`, `aviability`) 
+                        VALUES (NULL, ?, ?, ?, ?, ?)";
+            }else   {
+                $sql = "INSERT INTO `snacks` (`id`, `file_path`, `name`, `price`, `description`, `aviability`) 
+                        VALUES (NULL, ?, ?, ?, ?, ?)";
+            }
+
+            $stmt = mysqli_prepare($link, $sql);
+            mysqli_stmt_bind_param($stmt, "sssss", $param_path, $name, $price, $description, $aviability);
+            
+            if (mysqli_stmt_execute($stmt)) {
+                $resMessage = array(
+                    "message" => "File uploaded successfully.",
+                );
+            } else {
+                $resMessage = array(
+                    "message" => "Error uploading file.",
+                );
+            }
+        } else {
+            $resMessage = array(
+                "message" => "Error uploading file: " . $_FILES["fileUpload"]["error"],
+            );
+        }
+    }
+}
+?>
+<div class="alert <?php echo isset($resMessage) ? ($resMessage['type'] == 'error' ? 'alert-danger' : 'alert-success') : ''; ?>">
+    <?php echo isset($resMessage) ? $resMessage['message'] : ''; ?>
+</div>
+<!-- if (move_uploaded_file($_FILES["fileUpload"]["tmp_name"], $target_file)) {
+    // Your code here
+} else {
+    $resMessage = array(
+       
+    );
+} -->
