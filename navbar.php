10ab9cc0916212181b96c35e4a005adbf7451615
pkr <kuldiprparmar9759@gmail.com>
2023-05-06 10:32:25 +0530

    added in cart

--- navbar.php
+++ navbar.php
@@ -83,20 +83,28 @@ session_start();
                     Catagory
                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a class="dropdown-item" href="/food2/catagory/gujarati.php">Gujrati Items</a>
+                    <a class="dropdown-item" href="/food2/catagory/gujrati.php">Gujrati Items</a>
                     <a class="dropdown-item" href='/food2/catagory/punjabi.php'>Punjabi Items</a>
                     <a class="dropdown-item" href='/food2/catagory/chinese.php'>Chinese Items</a>
                     <a class="dropdown-item" href='/food2/catagory/snacks.php'>Snacks</a>
                 </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" style="color:white" href="/food2/loginsystem/logout.php">LogOut</a>
             </li>
             </div>
         </ul>
         <div  class="cart" style="float:right" >
                <a class="cartbtn" href="/food2/cart/maincart.php"  style="background-color:#E56C23;"> <img style="  height:40px ; width:40px ;margin-right:20px;" src="https://media.istockphoto.com/id/469047076/vector/white-icon-of-a-shopping-cart-on-orange-background.jpg?s=170667a&w=0&k=20&c=5OH1VrFlZ-Ndu1fL1h_ha_1p2hfPRIXPE1ZciKSVpXI="></a>
                 <div class="ind">
-                       <?php 
-                       echo $_SESSION['totalItems'];
-                       ?>
+                <?php 
+                     $count=0;
+                     foreach( $_SESSION['cart'] as $item)
+                        {
+                          $count++;
+                        }
+                          echo $count;
+                ?>
                 </div>
         </div>
         
